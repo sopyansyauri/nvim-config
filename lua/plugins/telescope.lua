f7caa90f06084c6cb14e9d492e8c63343329c83f
sopyansyauri <rifai.sopyan04@gmail.com>
2026-07-03 04:44:40 +0700

    menambahkan perintah go to definition

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,5 +11,6 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = 'Telescope got to definition'})
     end
 }
